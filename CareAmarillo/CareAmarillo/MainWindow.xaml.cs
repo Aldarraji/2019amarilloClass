7cff970c8d2cdc515af0a8124b441a94282553bb
Aldarraji <aramaldarraji@gmail.com>
2019-12-03 23:40:35 -0600

    Main page change

--- CareAmarillo/CareAmarillo/MainWindow.xaml.cs
+++ CareAmarillo/CareAmarillo/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace CareAmarillo
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
