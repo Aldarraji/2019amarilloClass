a4f1cff88c40f6bc178cc1909761fdfd4105bc85
Alex <Alex@ALEX-PC>
2019-12-09 16:11:07 -0600

    changes to page 3

--- CareAmarillo/CareAmarillo/Page3.xaml.cs
+++ CareAmarillo/CareAmarillo/Page3.xaml.cs
@@ -27,13 +27,18 @@ namespace CareAmarillo
         private void BtnRegister_Click(object sender, RoutedEventArgs e)
         {
             //create account 
+            if (txtPName.Text == "" | txtID.Text == "" | txtPW.Text == "" | txtEmail.Text == "" | txtAddress.Text == "" | txtPhone.Text == "")
+            {
+                MessageBox.Show("Please enter all the required information.");
+            }
+            else
+            {
+                MessageBox.Show("You have registered successfully. \n Please press ok then log in.");
+                this.Hide();
+                MainWindow MainWindow = new MainWindow();
+                MainWindow.Show();
+            }
 
-            //show a message box to conferm the regestration
-            MessageBox.Show("You have registered successfully. \n Please press ok then log in.");
-            //take back to the main page to log in
-            this.Hide();
-            MainWindow MainWindow = new MainWindow();
-            MainWindow.Show();
         }
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
