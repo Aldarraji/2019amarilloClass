282f07637e4736083edf7a53b3cba1067144db2f
wajeht <zombyard@gmail.com>
2019-12-10 14:41:12 -0600

    SQL changes for page 3

--- CareAmarillo/CareAmarillo/Page3.xaml.cs
+++ CareAmarillo/CareAmarillo/Page3.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.Data.SqlClient;
+using System.Data.Sql;
+
 namespace CareAmarillo
 {
     /// <summary>
@@ -19,20 +22,26 @@ namespace CareAmarillo
     /// </summary>
     public partial class Page3 : Window
     {
+        SqlConnection connection = new SqlConnection();
         public Page3()
         {
             InitializeComponent();
+            connection.ConnectionString = "Server=cis1.actx.edu;Database=project1;user id=db1;Password=db10;";
+            connection.Open();
+
         }
 
         private void BtnRegister_Click(object sender, RoutedEventArgs e)
         {
             //create account 
-            if (txtPName.Text == "" | txtID.Text == "" | txtPW.Text == "" | txtEmail.Text == "" | txtAddress.Text == "" | txtPhone.Text == "")
+            if (txtPName.Text == "" | txtZipCode.Text == "" | txtEmail.Text == "" | txtAddress.Text == "" | txtPhone.Text == "" | txtBed.Text == "" | txtCity.Text == "" | txtState.Text == "")
             {
                 MessageBox.Show("Please enter all the required information.");
             }
             else
             {
+                //var zipcode = Int32.Parse(txtZipCode.Text);
+                AddProvider();
                 MessageBox.Show("You have registered successfully. \n Please press ok then log in.");
                 this.Hide();
                 MainWindow MainWindow = new MainWindow();
@@ -54,6 +63,23 @@ namespace CareAmarillo
             this.Hide();
             UpdateInfo update = new UpdateInfo();
             update.Show();
+        }
+
+        private void AddProvider()
+        {
+            using (SqlCommand insertNewProvider = connection.CreateCommand())
+            {
+                insertNewProvider.CommandText = "insert into Shelter values (@Name, @Address, @ZipCode, @City, @State, @Phone, @Email, @BedVacancy);";
+                insertNewProvider.Parameters.Add(new SqlParameter("Name", txtPName.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("Address", txtAddress.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("ZipCode", txtZipCode.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("City", txtCity.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("State", txtState.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("Phone", txtPhone.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("Email", txtEmail.Text));
+                insertNewProvider.Parameters.Add(new SqlParameter("BedVacancy", txtBed.Text));
+                insertNewProvider.ExecuteNonQuery();
+            }
         }
     }
 }
