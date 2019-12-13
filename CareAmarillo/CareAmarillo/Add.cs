572b29268eaf240a9b9bea79f76dfd631c99ca61
AndrewSlaughter-117 <andrewslaughter00@gmail.com>
2019-12-13 14:39:24 -0600

    added and possibly got working adding, deleting and updating users. just needs attached to pages.

---
+++ CareAmarillo/CareAmarillo/Add.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace CareAmarillo
+{
+    class Add
+    {
+        static SqlConnection connection = new SqlConnection();
+        public Add()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.ConnectionString = "Server=cis1.actx.edu;Database=project1;User Id=db1;Password=db10;";
+                connection.Open();
+            }
+        }
+
+        private void AddHumanService(string fName, string lName, string email, string userID, string password)
+        {
+            using (SqlCommand insertNewProvider = connection.CreateCommand())
+            {
+                insertNewProvider.CommandText = "insert into Person values (@ID, @FirstName, @LastName, @Email, @TypeID, @UserID, @ShelterID, @Password);";
+                insertNewProvider.Parameters.Add(new SqlParameter("FirstName", fName));
+                insertNewProvider.Parameters.Add(new SqlParameter("LastName", lName));
+                insertNewProvider.Parameters.Add(new SqlParameter("Email", email));
+                insertNewProvider.Parameters.Add(new SqlParameter("TypeID", 3));
+                insertNewProvider.Parameters.Add(new SqlParameter("UserID", userID));
+                insertNewProvider.Parameters.Add(new SqlParameter("Password", password));
+                insertNewProvider.ExecuteNonQuery();
+            }
+        }
+
+        private void AddProvider(string name, string address, string zipCode, string city, string state, string phone, string email, string bedVacancy)
+        {
+            using (SqlCommand insertNewProvider = connection.CreateCommand())
+            {
+                insertNewProvider.CommandText = "insert into Shelter values (@Name, @Address, @ZipCode, @City, @State, @Phone, @Email, @BedVacancy);";
+                insertNewProvider.Parameters.Add(new SqlParameter("Name", name));
+                insertNewProvider.Parameters.Add(new SqlParameter("Address", address));
+                insertNewProvider.Parameters.Add(new SqlParameter("ZipCode", zipCode));
+                insertNewProvider.Parameters.Add(new SqlParameter("City", city));
+                insertNewProvider.Parameters.Add(new SqlParameter("State", state));
+                insertNewProvider.Parameters.Add(new SqlParameter("Phone", phone));
+                insertNewProvider.Parameters.Add(new SqlParameter("Email", email));
+                insertNewProvider.Parameters.Add(new SqlParameter("BedVacancy", bedVacancy));
+                insertNewProvider.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ CareAmarillo/CareAmarillo/Update.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace CareAmarillo
+{
+    class Update
+    {
+        static SqlConnection connection = new SqlConnection();
+        public Update()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.ConnectionString = "Server=cis1.actx.edu;Database=project1;User Id=db1;Password=db10;";
+                connection.Open();
+            }
+        }
+
+        static void UpdateAUser(string userID, string password, string fName, string lName, string email, string phone)
+        {
+            using (SqlCommand updateProf = connection.CreateCommand())
+            {
+                updateProf.CommandText = "update LogOn set Password = @password where ID = @userID ";
+                updateProf.CommandText += "update Person set FirstName = @firstName where UserID = @userID ";
+                updateProf.CommandText += "update Person set LastName = @lastName where UserID = @userID ";
+                updateProf.CommandText += "update Person set Email = @email where UserID = @userID ";
+                updateProf.CommandText += "update Person set Phone = @phone where UserID = @userID;";
+                updateProf.Parameters.Add(new SqlParameter("Password", password));
+                updateProf.Parameters.Add(new SqlParameter("firstName", fName));
+                updateProf.Parameters.Add(new SqlParameter("lastName", lName));
+                updateProf.Parameters.Add(new SqlParameter("email", email));
+                updateProf.Parameters.Add(new SqlParameter("phone", phone));
+                updateProf.Parameters.Add(new SqlParameter("userID", userID));
+                updateProf.ExecuteNonQuery();
+            }
+        }
+
+        static void DeleteAUser(string userID)
+        {
+            using (SqlCommand deleteUser = connection.CreateCommand())
+            {
+                deleteUser.CommandText = "delete from Person where UserID = @userID;";
+                deleteUser.Parameters.Add(new SqlParameter("userID", userID));
+                deleteUser.ExecuteNonQuery();
+            }
+            using (SqlCommand deleteUser = connection.CreateCommand())
+            {
+                deleteUser.CommandText = "delete from LogOn where ID = @ID;";
+                deleteUser.Parameters.Add(new SqlParameter("ID", userID));
+                deleteUser.ExecuteNonQuery();
+            }
+        }
+    }
+}
